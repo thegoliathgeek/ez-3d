b9bc31286c4dfb5ce2fbb0684e720fc4766d2dba
thegoliathgeek <dhanushdazz@gmail.com>
2025-04-03 17:25:11 +0530

    feat: project init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to EZ 3D</h1>
+      <div className="card">
+        <p>
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
